7ca581e19ab7977e33a926ca99e7c634cc37082c
zzx <835781192@qq.com>
2020-12-15 11:06:28 +0800

    路径细化

--- activitiTest/src/main/java/org/example/Utils/SecurityUtil.java
+++ activitiTest/src/main/java/org/example/Utils/SecurityUtil.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.Utils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- activitiTest/src/main/java/org/example/config/DemoApplicationConfiguration.java
+++ activitiTest/src/main/java/org/example/config/DemoApplicationConfiguration.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
